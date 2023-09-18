6f0f69a612a31f3dc979bdd007d7b719f1ef5644
Allan Silva <allandeveepode@gmail.com>
2023-09-18 21:15:03 +0000

    [ADD] scripts

---
+++ front_end.sh
@@ -0,0 +1,5 @@
+cd coinov/front-end;
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
+nvm install 16;
+npm run dev;
---
+++ ssh.sh
@@ -0,0 +1,2 @@
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y;
+cat ~/.ssh/id_rsa.pub;
