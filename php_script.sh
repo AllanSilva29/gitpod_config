5d1393baa8d45f2b7590f8a9b50de9af87289d38
AllanSilva29 <allanjulio61@gmail.com>
2023-09-23 21:04:11 -0300

    Update php_script.sh

--- php_script.sh
+++ php_script.sh
@@ -1,6 +1,11 @@
+### INSTALING PHP 7.4
 #apt -y install software-properties-common;
 #add-apt-repository ppa:ondrej/php;
+#yes y | apt-get install -y php7.4;
 #update-alternatives --config php;
+### INSTALING COMPOSER
+# curl -sS https://getcomposer.org/installer | php
+# php composer.phar update
 sudo apt update -y;
 sudo a2dismod php8.1 -y;
 sudo a2dismod mpm_prefork -y;
